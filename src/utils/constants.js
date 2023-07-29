997c6bdf0b6709efa0ac9b891264b09fe3ef11e3
Vs-kishu <91000916+Vs-kishu@users.noreply.github.com>
2023-07-29 11:50:51 +0530

    Used: explained CBC working

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,11 @@
+export const ALL_RESTRO =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.3176452&lng=82.9739144&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
 export const CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const LOGO_URL =
   "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
+
+export const RESTRO_INFO_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.3176452&lng=82.9739144&restaurantId=";
